1b9fe86d16743093c2477f94008dd58579110427
Maayan <damaayan@gmail.com>
2017-06-12 19:20:40 +0300

    first commit

--- EX3/scripts/Menu.js
+++ EX3/scripts/Menu.js
@@ -1,37 +1,21 @@
 ﻿
-document.addEventListener("DOMContentLoaded", function(event) {
-    var div = document.getElementById("nav_div");
-    div.innerHTML = "<nav class=\"navbar navbar-default\" role=\"navigation\">" +
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var navDivString = "<div id=\"nav_div\">" +
+        "<nav class=\"navbar navbar-default\" role=\"navigation\">" +
         "<div class=\"navbar-header\">" +
         "<a class=\"navbar-brand\" href=\"#\">Maze</a>" +
         "</div >" +
         "<div>" +
         "<ul class=\"nav navbar-nav\">" +
-        " <li class=\"active\"><a href=\"SingelPage.html\">Single Game</a></li>" +
-        " <li><a href=\"MultiPage.html\">Multiplayer Game</a></li>" +
+        "<li class=\"active\"><a href=\"SingelPage.html\">Single Game</a></li>" +
+        "<li><a href=\"MultiPage.html\">Multiplayer Game</a></li>" +
         "<li><a href=\"SettingsPage.html\">Settings</a></li>" +
         "<li><a href=\"RankingPage.html\">User Rankings</a></li>" +
         "<li style=\"margin-left:100px\"><a href=\"RegisterPage.html\">Register</a></li>" +
         "<li><a href=\"LoginPage.html\">Login</a></li>" +
         "</ul>" +
         "</div>" +
-        "</nav >";
+        "</nav >" +
+        "</div >";
+    $("body").prepend(navDivString);
 });
-//document.onload = function(event) {
-//    var div = document.getElementById("nav_div");
-//    div.innerHTML = "<nav class=\"navbar navbar-default\" role=\"navigation\">" +
-//        "<div class=\"navbar-header\">" +
-//        "<a class=\"navbar-brand\" href=\"#\">Maze</a>" +
-//        "</div >" +
-//        "<div>" +
-//        "<ul class=\"nav navbar-nav\">" +
-//        " <li class=\"active\"><a href=\"SingelPage.html\">Single Game</a></li>" +
-//        " <li><a href=\"MultiPage.html\">Multiplayer Game</a></li>" +
-//        "<li><a href=\"SettingsPage.html\">Settings</a></li>" +
-//        "<li><a href=\"RankingPage.html\">User Rankings</a></li>" +
-//        "<li style=\"margin-left:100px\"><a href=\"RegisterPage.html\">Register</a></li>" +
-//        "<li><a href=\"LoginPage.html\">Login</a></li>" +
-//        "</ul>" +
-//        "</div>" +
-//        "</nav >";
-//}
